efc451b04058c8724eda8ee2f0ea532a0a1e4f4d
hellfrom <hellfrom@mail.ru>
2014-06-02 15:49:32 +0300

    less global

--- js/app.js
+++ js/app.js
@@ -2,15 +2,16 @@ define([
     'jquery'
     ,'lodash'
     ,'backbone'
-    ,'L10n'
-    ,'lapter'
     ,'routers/routers'
+    //,'L10n'
+    //,'lapter'
     //,'mockAjax','mockUrl' //turn on ajax redirect
-], function($, _, Backbone, Lapter, L10n, Router){
+], function($, _, Backbone, Router/*, L10n, Lapter*/){
 	//set some important globals
 	window.GlobalEvents=window.GlobalEvents||_.extend({}, Backbone.Events);
-	window.L10n=L10n; //localization will be a global property
-    window.Lapter=Lapter;
+	window.globalData = window.globalData || {};
+	//window.L10n=L10n; //localization will be a global property
+    //window.Lapter=Lapter;
 	window.App={};
     var initialize = function(){
         window.App = new Router();
